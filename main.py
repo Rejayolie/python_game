994fd97f5bb44a73f09144f7b88a86efc3bfabc2
Rejoice Olie <rejayolie15@gmail.com>
2020-09-02 21:20:00 +0100

    first python game

---
+++ main.py
@@ -0,0 +1,8 @@
+print("Welcome to your first game!")
+name = (input("Please tell me your name? "))
+print(name)
+age = int(input("How old are you? "))
+if age <= 18:
+  print("You are not old enough to play, :(")
+  
+
